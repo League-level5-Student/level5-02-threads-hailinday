105b24acb9e801be00203a4fbf4c682bf0569e9e
hailinday <54258211+hailinday@users.noreply.github.com>
2023-05-14 13:00:31 -0700

    stuff

--- src/_03_Threaded_Greeting/ThreadedGreeter.java
+++ src/_03_Threaded_Greeting/ThreadedGreeter.java
@@ -1,5 +1,22 @@
 package _03_Threaded_Greeting;
 
-public class ThreadedGreeter implements  {
+public class ThreadedGreeter implements Runnable {
+	int num = 0;
+	public ThreadedGreeter(int n) {
+		num = n;
+	}
+	@Override
+	public void run() {
+		System.out.println("Hello from thread number: " + num);
+		if (num<50) {
+			Thread t = new Thread(new ThreadedGreeter(num+1));
+			t.start();
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
